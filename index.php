a96957d722f70444c46bb239bbe633120f612ddb
dev.mhrony <mh.rony.dhanvi@gmail.com>
2024-01-30 00:06:56 +0600

    first commit

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <H1>Hello Code Camp BD</H1>
+    <h6>Please Subscribe Our Channel</h6>
+
+    <div id="translate"></div>
+
+    <script src="http://translate.google.com/translate_a/element.js?cb=loadGoogleTranslate"></script>
+
+    <script>
+        function loadGoogleTranslate(){
+            new google.translate.TranslateElement("translate");
+        }
+    </script>
+
+
+</body>
+</html>
